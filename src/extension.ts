8a512403320e213c3141c136d935a7a4546dbeed
Audrey <audreyjezy@gmail.com>
2023-11-18 12:50:07 +0100

    feat: polgygot code has been copied over, and now it is time to clean it up

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 import { identifyCodebaseFramework } from './service/identity.service';
 import { processExpressProject } from './express/express-pocessor';
-import { identifyDjangoViews } from './django/django-views';
-import { generateDocumentation } from './service/assistant.service';
 import { authenticateWithGitHub } from './service/github.service';
+import { processDjangoProject } from './django/django-processor';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('kodekraftai.generateDocumentation', async () => {
@@ -37,11 +38,11 @@ export function activate(context: vscode.ExtensionContext) {
         let isLoggedIn;
 
         const session = await vscode.authentication.getSession('github', ['repo', 'workflow', 'read:user']);
-        if(session){
-            const isLoggedIn = true 
+        if (session) {
+            const isLoggedIn = true
 
-        }else{
-            const isLoggedIn = false 
+        } else {
+            const isLoggedIn = false
         }
 
         if (!isLoggedIn) {
@@ -55,42 +56,133 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
 
+    // Register the code action provider for specific languages
+    context.subscriptions.push(vscode.languages.registerCodeActionsProvider(
+        ['plaintext', 'python', 'javascript', 'java', 'typescript'],
+        new PolyglotCodeActionProvider()
+    ));
+
+    // Register the command to show the language menu
+    context.subscriptions.push(vscode.commands.registerCommand('kodekraftai.showLanguageMenu', () => {
+        // Get the entire document's text
+        const document = vscode.window.activeTextEditor?.document;
+        if (document) {
+            const entireCode = document.getText();
+
+            // Show a quick pick menu to choose the target language
+            vscode.window.showQuickPick(['JavaScript', 'Python', 'Java', 'TypeScript'])
+                .then((selectedLanguage) => {
+                    if (selectedLanguage) {
+                        try {
+                            // Replace the code with a placeholder code in the selected language
+                            const convertedCode = getConvertedCode(entireCode, selectedLanguage, document.fileName, document.languageId);
+                            vscode.window.activeTextEditor?.edit((editBuilder) => {
+                                editBuilder.replace(
+                                    new vscode.Range(
+                                        new vscode.Position(0, 0),
+                                        document.lineAt(document.lineCount - 1).rangeIncludingLineBreak.end
+                                    ),
+                                    convertedCode
+                                );
+                            });
+
+                            // Create a new file with the converted code
+                            createNewFile(document.fileName, selectedLanguage, convertedCode);
+                        } catch (error) {
+                            // Handle errors related to code conversion
+                            vscode.window.showErrorMessage(`Error converting code: ${error instanceof Error ? error.message : 'Unknown error'}`);
+                        }
+                    }
+                });
+        }
+    }));
+
     context.subscriptions.push(polygotDisposable);
     context.subscriptions.push(checkLoginCommand);
 
 }
+function getConvertedCode(code: string, targetLanguage: string, fileName: string, sourceLanguage: string): string {
+    // Extract the source file extension
+    const targetFileExtension = getTargetFileExtension(targetLanguage);
+    const sourceFileExtension = path.extname(fileName).slice(1);
 
-
-function getWebviewContent(panel: vscode.WebviewPanel) {
-    const extensionUri = vscode.extensions.getExtension('kodekraftai.kodekraftai')!.extensionUri;
-    const scriptUri = panel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'index.js'));
-
-    return `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>GitHub Login</title>
-        </head>
-        <body>
-            <h2>GitHub Login</h2>
-            <button onclick="login()">Login with GitHub</button>
-            <script src="${scriptUri}"></script>
-        </body>
-        </html>
-    `;
+    // Add logic to convert the code to the specified language
+    // For simplicity, I'll just return a placeholder string
+    return `// check file.${targetFileExtension} for the converted code\n\n// Converted from ${sourceLanguage} to ${targetLanguage}\n\n${code}`;
 }
+
+function createNewFile(originalFileName: string, targetLanguage: string, convertedCode: string): void {
+    // Get the directory and base name of the original file
+    const originalFileDir = path.dirname(originalFileName);
+    const originalFileNameWithoutExt = path.basename(originalFileName, path.extname(originalFileName));
+
+    // Create a new file name with the target language's extension
+    const targetFileExtension = getTargetFileExtension(targetLanguage);
+    const newFileName = `${originalFileNameWithoutExt}.${targetFileExtension}`;
+    const newFilePath = path.join(originalFileDir, newFileName);
+
+    try {
+        // Write the converted code to the new file
+        fs.writeFileSync(newFilePath, `// code converted from file.${path.extname(originalFileName).slice(1)} to file.${targetFileExtension}\n\n${convertedCode}`);
+
+        // Open the newly created file
+        vscode.workspace.openTextDocument(newFilePath).then((document) => {
+            vscode.window.showTextDocument(document);
+        });
+    } catch (error) {
+        // Handle errors related to file creation
+        vscode.window.showErrorMessage(`Error creating new file: ${error instanceof Error ? error.message : 'Unknown error'}`);
+    }
+}
+/**
+ * Represents the code action provider for the Polyglot extension.
+ */
+class PolyglotCodeActionProvider implements vscode.CodeActionProvider {
+    /**
+     * Provides code actions for the given document, range, and context.
+     *
+     * @param {vscode.TextDocument} document - The document in which the code action provider is invoked.
+     * @param {vscode.Range | vscode.Selection} range - The range for which code actions are requested.
+     * @param {vscode.CodeActionContext} context - The context for the code action request.
+     * @returns {vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]>} The list of code actions.
+     */
+    provideCodeActions(
+        document: vscode.TextDocument,
+        range: vscode.Range | vscode.Selection,
+        context: vscode.CodeActionContext
+    ): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
+        console.log('Code action provider invoked.');
+
+        // Offer a code action to convert the code to a specific language
+        const convertAction = new vscode.CodeAction('Kora AI: Convert Selected Code', vscode.CodeActionKind.QuickFix);
+        convertAction.command = {
+            title: 'Kora AI: Convert Selected Code',
+            command: 'kodekraftai.showLanguageMenu',
+        };
+        return [convertAction];
+    }
+}
+
+/**
+ * Gets the target file extension based on the selected target language.
+ *
+ * @param {string} targetLanguage - The selected target language.
+ * @returns {string} The corresponding file extension.
+ */
+function getTargetFileExtension(targetLanguage: string): string {
+    // Map target languages to file extensions
+    const languageExtensions: Record<string, string> = {
+        'JavaScript': 'js',
+        'Python': 'py',
+        'Java': 'java',
+        'TypeScript': 'ts',
+    };
+
+    // Default to using the language as the extension
+    return languageExtensions[targetLanguage] || targetLanguage.toLowerCase();
+}
+
 
 export function deactivate() { }
 
 
-async function processDjangoProject() {
-    const djangoUrlsFiles = await vscode.workspace.findFiles('**/urls.py');
-
-    for (const file of djangoUrlsFiles) {
-        const views = await identifyDjangoViews(file);
-        if (views) {
-            // Use the 'views' array to generate documentation or perform other tasks.
-            generateDocumentation(views);
-        }
-    }
-}
